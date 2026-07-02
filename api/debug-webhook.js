1601dea68ba26b0c95fd73b08ce667e1db1baba5
Marwahamdi301 Png <marwahamdi301@gmail.com>
2026-07-02 22:29:51 +0100

    chore: remove temp debug endpoints, register webhook via CLI instead

--- api/debug-webhook.js
+++
@@ -1,6 +0,0 @@
-export default async function handler(req, res) {
-  const token = process.env.TELEGRAM_BOT_TOKEN;
-  const r = await fetch(`https://api.telegram.org/bot${token}/getWebhookInfo`);
-  const data = await r.json();
-  res.status(200).json(data.result);
-}
